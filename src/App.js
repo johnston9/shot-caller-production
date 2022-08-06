4f6517c0a2ee58dd75c5b08e061ce5d107ff5c11
johnston9 <davejohnston9@gmail.com>
2022-08-06 21:46:14 +0000

    Add BootstrapReact

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Button from "react-bootstrap/Button";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>SHOT CALLER PRODUCTIONS</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
