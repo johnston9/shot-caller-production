e2c43e1d12b99f43c5ba7a974d63090bf41b84ff
johnston9 <davejohnston9@gmail.com>
2022-09-06 20:14:20 +0000

    Pro

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,8 +1,116 @@
-import React from 'react'
+import React from 'react';
+import styles from "../../styles/Profiles.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { Link } from 'react-router-dom';
+import Avatar from '../../components/Avatar';
+import Button from "react-bootstrap/Button";
+// import { useSetProfileData } from '../../contexts/ProfileDataContext';
+// import { useRedirect } from '../../hooks/Redirect';
+import Card from "react-bootstrap/Card";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-const Profile = () => {
+const Profile = (props) => {
+  const { profile, imageSize="40" } = props;
+  const { id, following_id, image, owner, name, company } = profile;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
   return (
-    <div>Profile</div>
+    <div>
+      {/* mine */}
+      <div className='mt-1 text-center'>
+            <Card className={styles.Back}>
+                <Card.Body className={`px-0 ${styles.CardHead}`} >
+                <Link to={`/profiles/${id}`}>
+                {/* Avatar */}
+                <Row>
+                <Col xs={12} >
+                    <Avatar src={image} height={imageSize}/>
+                </Col>
+                </Row>
+                <Row className='mt-2 text-center'>
+                    <Col xs={12} >   
+                    <div>
+                    <h5 className={`${styles.WordBreakWhite}`}
+                    style={{ textTransform: 'capitalize'}}> 
+                    {name} {owner}
+                    </h5>
+                    </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} >  
+                    <p style={{ textTransform: 'capitalize'}} 
+                    className={` ${styles.WordBreakWhite}`} >
+                    {company} </p>
+                    </Col>
+                </Row>
+                </Link>
+                </Card.Body>
+                {/* <Card.Header className="my-0 py-1 px-0"> 
+                <Row>
+                    <Col xs={12}>
+                    <div className="mt-1" >
+                    {currentUser &&
+                    !is_owner &&
+                    (following_id ? (
+                        <Button
+                        className={` py-0 ${btnStyles.Button} ${btnStyles.White}`}
+                        onClick={() => handleUnfollow(profile)}
+                        >
+                        unfollow
+                        </Button>
+                    ) : (
+                        <Button
+                        className={`py-0 ${btnStyles.Button} ${btnStyles.Black}`}
+                        onClick={() => handleFollow(profile) }
+                        >
+                        follow
+                        </Button>
+                    ))}
+                    </div>
+                    </Col>
+                </Row>
+                </Card.Header> */}
+            </Card>
+        </div>
+        {/* his */}
+        <div
+      className={`my-3 d-flex align-items-center`}
+    >
+      <div>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
+        <strong>{owner}</strong>
+      </div>
+      <div className={`text-right`}>
+        {
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+              onClick={() => {}}
+            >
+              unfollow
+            </Button>
+          ) : (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Black}`}
+              onClick={() => {}}
+            >
+              follow
+            </Button>
+          ))}
+      </div>
+    </div>
+    </div>
   )
 }
 
--- src/pages/profiles/ProfilesPage.js
+++ src/pages/profiles/ProfilesPage.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { axiosReq } from "../../api/axiosDefaults";
+import Asset from "../../components/Asset";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Profile from "./Profile";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 const ProfilesPage = () => {
-  return (
-    <div>ProfilesPage</div>
-  )
-}
+  const [profileData, setProfileData] = useState({
+    profileOne: { results: [] },
+    profilesAll: { results: [] },
+  });
+  const { profilesAll } = profileData;
+  const currentUser = useCurrentUser();
 
-export default ProfilesPage
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          profilesAll: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
+  return (
+    <div className={`mt-3 mx-3`}>
+      <Row>
+      {profilesAll.results.length ? (
+        <>
+          {profilesAll.results.map((profile) => (
+            <Col xs={6} sm={4} md={3} lg={2} className="px-2 py-2 p-0 p-lg-2">
+            <Profile key={profile.id} profile={profile} />
+            </Col>
+          ))}
+        </>
+      ) : (
+        <Asset spinner />
+      )}
+      </Row>
+    </div>
+  );
+};
+
+export default ProfilesPage;
